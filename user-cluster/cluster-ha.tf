230af71edaa1594519803b0590efcac32a95fb25
Taylor Price <tayworm@gmail.com>
2019-09-23 11:20:38 -0700

    update user cluster

--- user-cluster/cluster-ha.tf
+++ user-cluster/cluster-ha.tf
@@ -3,8 +3,9 @@ resource "rancher2_cluster" "user-cluster" {
   description = "TF managed RKE cluster"
 
   rke_config {
-    # kubernetes_version = "v1.13.9-rancher1-2"
-    kubernetes_version = "v1.14.5-rancher1-1"
+    # Shows how easy it is to update
+    kubernetes_version = "v1.13.9-rancher1-2"
+    # kubernetes_version = "v1.14.5-rancher1-1"
     cloud_provider {
       name = "aws"
     }
