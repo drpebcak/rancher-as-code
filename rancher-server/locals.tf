8f19a8fb77578e003e7daa4678be0532facae16e
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-26 10:58:12 -0600

    move locals to locals.tf

---
+++ rancher-server/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  name              = "rancher-demo"
+  rancher_version   = "v2.2.8"
+  le_email          = "none@none.com"
+  domain            = "eng.rancher.space"
+  instance_type     = "t3.large"
+  master_node_count = 3
+  worker_node_count = 3
+}
--- rancher-server/main.tf
+++ rancher-server/main.tf
@@ -6,13 +6,3 @@ terraform {
     }
   }
 }
-
-locals {
-  name              = "rancher-demo"
-  rancher_version   = "v2.2.8"
-  le_email          = "none@none.com"
-  domain            = "eng.rancher.space"
-  instance_type     = "t3.large"
-  master_node_count = 3
-  worker_node_count = 3
-}
