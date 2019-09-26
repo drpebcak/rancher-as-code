a53df509afd332a69cde151aac82c6b5eed12afb
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-26 10:54:55 -0600

    Move variables to variables.tf file

--- rancher-server/main.tf
+++ rancher-server/main.tf
@@ -16,15 +16,3 @@ locals {
   master_node_count = 3
   worker_node_count = 3
 }
-
-variable "rancher_password" {
-  type = string
-}
-
-variable "github_client_id" {
-  type = string
-}
-
-variable "github_client_secret" {
-  type = string
-}
---
+++ rancher-server/variables.tf
@@ -0,0 +1,11 @@
+variable "rancher_password" {
+  type = string
+}
+
+variable "github_client_id" {
+  type = string
+}
+
+variable "github_client_secret" {
+  type = string
+}
