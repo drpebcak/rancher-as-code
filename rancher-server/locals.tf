932e8d0b0c5f71a8f386f5ccb77a28cb7ce08a13
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-27 22:06:55 -0600

    parameterize more local constants

--- rancher-server/locals.tf
+++ rancher-server/locals.tf
@@ -1,9 +1,9 @@
 locals {
   name              = "rancher-demo"
   rancher_version   = "v2.2.8"
-  le_email          = "none@none.com"
-  domain            = "eng.rancher.space"
-  instance_type     = "t3.large"
-  master_node_count = 3
-  worker_node_count = 3
+  le_email          = var.le_email
+  domain            = var.domain
+  instance_type     = var.instance_type
+  master_node_count = var.master_node_count
+  worker_node_count = var.worker_node_count
 }
--- rancher-server/variables.tf
+++ rancher-server/variables.tf
@@ -10,6 +10,31 @@ variable "github_client_secret" {
   type = string
 }
 
+variable "le_email" {
+  type        = string
+  default     = "none@none.com"
+  description = "LetsEncrypt email address to use"
+}
+
+variable "domain" {
+  type    = string
+  default = "eng.rancher.space"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.large"
+}
+
+variable "master_node_count" {
+  type    = number
+  default = 3
+}
+
+variable "worker_node_count" {
+  type    = number
+  default = 3
+}
 
 variable "instance_ssh_user" {
   type        = string
