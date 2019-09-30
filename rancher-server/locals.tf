615e282cc69ed18134c2cdb3d639970ff9c28648
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-30 15:40:42 -0600

    Determine rke backup config

--- rancher-server/locals.tf
+++ rancher-server/locals.tf
@@ -12,6 +12,11 @@ locals {
   rancher2_auth_github_org            = length(var.rancher_github_auth_org) > 0 ? [var.rancher_github_auth_org] : []
   rancher2_auth_github_team           = length(var.rancher_github_auth_team) > 0 ? [var.rancher_github_auth_team] : []
   rancher2_auth_github_principal_list = concat(local.rancher2_auth_github_user, local.rancher2_auth_github_org, local.rancher2_auth_github_team)
+  # Default to main aws region for backups unless overridden
+  rke_backup_region = length(var.rke_backups_region) > 0 ? var.rke_backups_region : var.aws_region
+  # Default to S3 endpoint for region unless overridden
+  rke_backup_endpoint = length(var.rke_backups_s3_endpoint) > 0 ? var.rke_backups_s3_endpoint : "s3.${local.rke_backup_region}.amazonaws.com"
+
   # If not using default vpc in region, use vpc_id passed in
   vpc_id = var.use_default_vpc ? data.aws_vpc.default.id : var.vpc_id
 }
