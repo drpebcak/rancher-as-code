c970913423d5c1e563916c855a990fd4997afb53
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-30 10:39:43 -0600

    format

--- rancher-server/locals.tf
+++ rancher-server/locals.tf
@@ -1,10 +1,11 @@
 locals {
-  name                              = "rancher-demo"
-  rancher_version                   = "v2.2.8"
-  le_email                          = var.le_email
-  domain                            = var.domain
-  instance_type                     = var.instance_type
-  master_node_count                 = var.master_node_count
-  worker_node_count                 = var.worker_node_count
-  rancher2_auth_config_github_count = var.rancher_github_auth_enabled ? 1 : 0
+  name              = "rancher-demo"
+  rancher_version   = "v2.2.8"
+  le_email          = var.le_email
+  domain            = var.domain
+  instance_type     = var.instance_type
+  master_node_count = var.master_node_count
+  worker_node_count = var.worker_node_count
+
+  rancher2_auth_config_github_count   = var.rancher_github_auth_enabled ? 1 : 0
 }
