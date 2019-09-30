d9f6c33a7cf23d94141a06f53280ec8a02ae705e
Jeff Bachtel <jeff.bachtel@ironnetcybersecurity.com>
2019-09-30 12:27:21 -0600

    fix, correct function name
    too much Python

--- rancher-server/locals.tf
+++ rancher-server/locals.tf
@@ -8,8 +8,8 @@ locals {
   worker_node_count = var.worker_node_count
 
   rancher2_auth_config_github_count   = var.rancher_github_auth_enabled ? 1 : 0
-  rancher2_auth_github_user           = len(var.rancher_github_auth_user) > 0 ? [var.rancher_github_auth_user] : []
-  rancher2_auth_github_org            = len(var.rancher_github_auth_org) > 0 ? [var.rancher_github_auth_org] : []
-  rancher2_auth_github_team           = len(var.rancher_github_auth_team) > 0 ? [var.rancher_github_auth_team] : []
+  rancher2_auth_github_user           = length(var.rancher_github_auth_user) > 0 ? [var.rancher_github_auth_user] : []
+  rancher2_auth_github_org            = length(var.rancher_github_auth_org) > 0 ? [var.rancher_github_auth_org] : []
+  rancher2_auth_github_team           = length(var.rancher_github_auth_team) > 0 ? [var.rancher_github_auth_team] : []
   rancher2_auth_github_principal_list = concat(local.rancher2_auth_github_user, local.rancher2_auth_github_org, local.rancher2_auth_github_team)
 }
